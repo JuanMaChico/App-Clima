86f6ed82b8fa4721bd4575ccbc0efd06c82defa3
JuanMaChico <juanmanuelchico@hotmail.com>
2023-04-16 18:34:41 -0300

    Formated code

--- src/app/App.styled.jsx
+++ src/app/App.styled.jsx
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Root = styled.div`
 	border-style: solid;
@@ -10,20 +10,12 @@ export const Root = styled.div`
 	justify-content: center;
 	align-content: center;
 	min-width: 820px;
-    
+
 	-webkit-box-shadow: -1px 32px 38px 0px rgba(0, 0, 0, 0.75);
 	-moz-box-shadow: -1px 32px 38px 0px rgba(0, 0, 0, 0.75);
 	box-shadow: -1px 32px 38px 0px rgba(0, 0, 0, 0.75);
 
-	background: #8360c3; /* fallback for old browsers */
-	background: -webkit-linear-gradient(
-		to right,
-		#2ebf91,
-		#8360c3
-	); /* Chrome 10-25, Safari 5.1-6 */
-	background: linear-gradient(
-		to right,
-		#2ebf91,
-		#8360c3
-	); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	background: #8360c3;
+	background: -webkit-linear-gradient(to right, #2ebf91, #8360c3);
+	background: linear-gradient(to right, #2ebf91, #8360c3);
 `;
